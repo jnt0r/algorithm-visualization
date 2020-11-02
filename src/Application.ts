0fac2e37cdddbc3830a31908673a32876384d290
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-11-02 18:52:07 +0100

    refactor

--- src/Application.ts
+++ src/Application.ts
@@ -1,55 +1,57 @@
 import Renderer from './display/Renderer';
-import SortingProblem from './problems/sorting/SortingProblem';
+import SortingProblem, { BubbleSort, ProblemSolver, SelectionSort } from './problems/sorting/SortingProblem';
 import PathFindingProblem from './problems/pathfinding/PathFindingProblem';
 import Problem from './problems/Problem';
 
 export default class Application {
-    private readonly renderer;
-    private readonly problems: Record<string, { new (): Problem }> = {};
-    private problem: Problem;
+    private readonly renderer: Renderer = new Renderer();
+    private readonly problems: Record<string, Problem> = {};
+    private readonly algorithms: Record<string, ProblemSolver[]> = {};
+    private problem!: Problem;
 
     private readonly problemSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById('problemSelect');
     private readonly algorithmSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById('algorithmSelect');
+    private readonly solveBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById('solveBtn');
+    private readonly generateBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById('generateBtn');
 
     constructor() {
-        this.renderer = new Renderer();
-
         // Adding Problems to be able to choose from
-        this.problems['Sorting'] = SortingProblem;
-        this.problems['Path-finding'] = PathFindingProblem;
+        this.problems['Sorting'] = new SortingProblem();
+        this.algorithms['Sorting'] = [new BubbleSort(), new SortingProblem()];
+        this.problems['Path-finding'] = new PathFindingProblem();
+        this.algorithms['Path-finding'] = [];
 
         // Set default problem to 'Sorting'
         this.setProblem('Sorting');
     }
 
     run(): void {
-        // this.problem.render(this.renderer);
-
         for (const p in this.problems) {
             this.problemSelect.appendChild(new Option(p, p));
         }
-        this.problemSelect.onchange = () => {
-            this.setProblem(this.problemSelect.value);
-        };
+        this.problemSelect.onchange = () => this.setProblem(this.problemSelect.value);
 
-        document.getElementById('solveBtn')!.onclick = () => {
+        this.solveBtn.onclick = () => {
             this.problem.solve(this.renderer).then(() => console.log('solved'));
         };
 
-        document.getElementById('generateBtn')!.onclick = () => {
-            this.problem.generate();
-            this.problem.render(this.renderer);
+        this.generateBtn.onclick = () => {
+            this.regenerateProblem();
         };
     }
 
+    private regenerateProblem() {
+        this.problem.generate();
+        this.problem.render(this.renderer);
+    }
+
     private setProblem(problem: string) {
-        this.problem = new this.problems[problem]();
+        this.problem = this.problems[problem];
 
         this.clearAlgorithmSelectField();
         this.fillAlgorithmSelectField();
 
-        this.problem.generate();
-        this.problem.render(this.renderer);
+        this.regenerateProblem();
     }
 
     private fillAlgorithmSelectField() {
