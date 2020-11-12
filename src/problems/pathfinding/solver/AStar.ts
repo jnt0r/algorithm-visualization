287573842e908c8dd1324622840d694e1fcd72cf
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-11-12 19:33:32 +0100

    refactor

--- src/problems/pathfinding/solver/AStar.ts
+++ src/problems/pathfinding/solver/AStar.ts
@@ -15,34 +15,46 @@ export default class AStar implements PathFindingProblemSolver {
         this.cameFrom = [];
 
         while (this.openSet.length !== 0) {
-            const current = this.openSet.sort(
-                (a, b) => a.cost + this.getDistanceToGoal(a) - (b.cost + this.getDistanceToGoal(b)),
-            )[0];
-            this.openSet.splice(this.openSet.indexOf(current), 1);
+            const current: Box = this.getBestBoxFromOpenSet();
             current.visited = true;
 
             if (current === grid.goal) {
                 this.constructPath();
-                break;
+                return; // Path found. Stop execution
             }
 
             await renderer.animate(() => {
                 const x = current.ax;
                 const y = current.ay;
-                this.processNeighbour(grid.getElement(x + 1, y), current);
-                this.processNeighbour(grid.getElement(x - 1, y), current);
                 this.processNeighbour(grid.getElement(x, y + 1), current);
+                this.processNeighbour(grid.getElement(x + 1, y), current);
                 this.processNeighbour(grid.getElement(x, y - 1), current);
+                this.processNeighbour(grid.getElement(x - 1, y), current);
             });
-            console.log([...this.openSet]);
         }
-        console.log('finished');
-        grid.goal.markGoal();
+        // No path has been found
+        alert('no path found');
+    }
+
+    private constructPath(): void {
+        let current = this.grid.goal;
+        current = this.cameFrom[current.ax][current.ay];
+        while (current !== this.grid.start) {
+            current.markPartOfPath();
+            current = this.cameFrom[current.ax][current.ay];
+        }
+        this.grid.goal.markGoal();
+    }
+
+    private getBestBoxFromOpenSet(): Box {
+        this.openSet.sort((a, b) => a.cost + this.getDistanceToGoal(a) - (b.cost + this.getDistanceToGoal(b)));
+        return this.openSet.splice(0, 1)[0];
     }
 
     private processNeighbour(neighbour: Box | undefined, current: Box) {
         if (neighbour && !neighbour.visited) {
             neighbour.markVisited();
+
             const costFromStart = current.cost + 1;
             if (costFromStart < neighbour.cost) {
                 if (!this.cameFrom[neighbour.ax]) {
@@ -62,16 +74,5 @@ export default class AStar implements PathFindingProblemSolver {
         const dx = Math.pow(this.grid.goal.ax - element.ax, 2);
         const dy = Math.pow(this.grid.goal.ay - element.ay, 2);
         return Math.sqrt(dx + dy);
-    }
-
-    private constructPath(): void {
-        console.log('found Path');
-
-        let current = this.grid.goal;
-        current = this.cameFrom[current.ax][current.ay];
-        while (current !== this.grid.start) {
-            current.markPartOfPath();
-            current = this.cameFrom[current.ax][current.ay];
-        }
     }
 }
