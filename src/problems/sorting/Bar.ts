89ef7487872ccf46014a4b5d7d24d56aef4f045f
jnt0r <38696668+jnt0r@users.noreply.github.com>
2020-12-20 13:21:18 +0100

    add setColor wrapper method

--- src/problems/sorting/Bar.ts
+++ src/problems/sorting/Bar.ts
@@ -10,21 +10,25 @@ export default class Bar implements Renderable {
 
     constructor(private readonly id: number, private readonly value: number, private readonly renderer: Renderer) {
         this.rectangle = renderer.createRectangle(new Point(100 + id * 25, 100), 20, value);
-        this.rectangle.setColor(this.defaultColor);
+        this.setColor(this.defaultColor);
+    }
+
+    setColor(hexCode: string): void {
+        this.rectangle.setColor(hexCode);
     }
 
     markRed(): void {
-        this.rectangle.setColor('#FF4949');
+        this.setColor('#FF4949');
     }
 
     setSorted(): void {
         this.sorted = true;
-        this.rectangle.setColor('#13CE66');
+        this.setColor('#13CE66');
     }
 
     unmark(): void {
         if (!this.sorted) {
-            this.rectangle.setColor(this.defaultColor);
+            this.setColor(this.defaultColor);
         } else {
             this.setSorted();
         }
