5a49ff074213b3604bb9b5544738b6b43acacebc
jnt0r <38696668+jnt0r@users.noreply.github.com>
2021-01-03 08:38:25 +0100

    add test for SortableData

--- test/problems/SortingProblems.spec.ts
+++ test/problems/SortingProblems.spec.ts
@@ -7,6 +7,92 @@ import { TestRenderer } from '../TestRenderer';
 
 const renderer = new TestRenderer();
 
+describe('stats', () => {
+    test('Comparisons is initialized with 0', () => {
+        const data = new SortableData([1, 2, 3, 4, 5], renderer);
+        expect(data.getStats().getStats().get('comparisons')).toEqual(0);
+    });
+    test('Comparisons is increased by 1 on compare', () => {
+        const data = new SortableData([1, 2, 3, 4, 5], renderer);
+        data.compareElements(1, '=', 2);
+        expect(data.getStats().getStats().get('comparisons')).toEqual(1);
+    });
+    test('Swaps is initialized with 0', () => {
+        const data = new SortableData([1, 2, 3, 4, 5], renderer);
+        expect(data.getStats().getStats().get('swaps')).toEqual(0);
+    });
+    test('Swaps is increased by 1 on swap', () => {
+        const data = new SortableData([1, 2, 3, 4, 5], renderer);
+        data.swap(1, 2);
+        expect(data.getStats().getStats().get('swaps')).toEqual(1);
+    });
+});
+
+describe('Compare', () => {
+    const data = new SortableData([0, 1, 2, 3, 4], renderer);
+
+    test('1 = 2 is false', () => {
+        expect(data.compareElements(1, '=', 2)).toBeFalsy();
+    });
+    test('1 = 1 is true', () => {
+        expect(data.compareElements(1, '=', 1)).toBeTruthy();
+    });
+
+    test('1 < 2 is true', () => {
+        expect(data.compareElements(1, '<', 2)).toBeTruthy();
+    });
+    test('2 < 1 is false', () => {
+        expect(data.compareElements(2, '<', 1)).toBeFalsy();
+    });
+    test('1 < 1 is false', () => {
+        expect(data.compareElements(1, '<', 1)).toBeFalsy();
+    });
+
+    test('1 <= 2 is true', () => {
+        expect(data.compareElements(1, '<=', 2)).toBeTruthy();
+    });
+    test('2 <= 1 is false', () => {
+        expect(data.compareElements(2, '<=', 1)).toBeFalsy();
+    });
+    test('1 <= 1 is true', () => {
+        expect(data.compareElements(1, '<=', 1)).toBeTruthy();
+    });
+
+    test('2 > 1 is true', () => {
+        expect(data.compareElements(2, '>', 1)).toBeTruthy();
+    });
+    test('1 > 2 is false', () => {
+        expect(data.compareElements(1, '>', 2)).toBeFalsy();
+    });
+    test('1 > 1 is false', () => {
+        expect(data.compareElements(1, '>', 1)).toBeFalsy();
+    });
+
+    test('2 >= 1 is true', () => {
+        expect(data.compareElements(2, '>=', 1)).toBeTruthy();
+    });
+    test('1 >= 2 is false', () => {
+        expect(data.compareElements(1, '>=', 2)).toBeFalsy();
+    });
+    test('1 >= 1 is true', () => {
+        expect(data.compareElements(1, '>=', 1)).toBeTruthy();
+    });
+});
+
+describe('Swap', () => {
+    const data = new SortableData([0, 1, 2, 3, 4], renderer);
+
+    test('should swap given indexes', () => {
+        expect(data.getElement(4).getValue()).toEqual(4);
+        expect(data.getElement(2).getValue()).toEqual(2);
+
+        data.swap(2, 4);
+
+        expect(data.getElement(4).getValue()).toEqual(2);
+        expect(data.getElement(2).getValue()).toEqual(4);
+    });
+});
+
 const sortedData = new SortableData([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], renderer);
 test('creation', () => {
     expect(sortedData).toBeDefined();
