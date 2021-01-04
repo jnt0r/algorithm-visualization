cb74795f65d782967cb1fde7e0d5f37ddaa568aa
jnt0r <38696668+jnt0r@users.noreply.github.com>
2021-01-04 21:17:05 +0100

    Add tests for point.ts

---
+++ test/renderer/point.spec.ts
@@ -0,0 +1,19 @@
+import Point from '../../src/renderer/Point';
+
+describe('Point', () => {
+    test('distanceTo should return euclidean distance #1', () => {
+        const pointA = new Point(0, 0);
+        const pointB = new Point(3, 4);
+
+        expect(pointA.distanceTo(pointB)).toBe(5);
+        expect(pointB.distanceTo(pointA)).toBe(5);
+    });
+
+    test('distanceTo should return euclidean distance #2', () => {
+        const pointA = new Point(15, 3);
+        const pointB = new Point(9, 21);
+
+        expect(pointA.distanceTo(pointB)).toBe(18.973665961010276);
+        expect(pointB.distanceTo(pointA)).toBe(18.973665961010276);
+    });
+});
