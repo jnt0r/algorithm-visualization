55d7b41c22d13da36c26647a898157f2414d2789
jnt0r <38696668+jnt0r@users.noreply.github.com>
2021-01-07 10:36:37 +0100

    create PathFindingProblems.spec.ts

---
+++ test/problems/PathFindingProblems.spec.ts
@@ -0,0 +1,27 @@
+import Grid from '../../src/problems/pathfinding/Grid';
+import { TestRenderer } from '../TestRenderer';
+
+const renderer = new TestRenderer();
+
+describe('stats', () => {
+    test('Visited fields should be initialized with 0', () => {
+        const data = new Grid(10, 10, renderer);
+        expect(data.getStats().getStats().get('Visited fields')).toEqual(0);
+    });
+
+    test('Visited fields should increase by 1 on getElement', () => {
+        const data = new Grid(10, 10, renderer);
+        data.getElement(1, 1);
+        expect(data.getStats().getStats().get('Visited fields')).toEqual(1);
+    });
+
+    test('Path fields should be initialized with 0', () => {
+        const data = new Grid(10, 10, renderer);
+        expect(data.getStats().getStats().get('Path fields')).toEqual(0);
+    });
+
+    test('Path fields should increase by 1 per Path field', () => {
+        const data = new Grid(10, 10, renderer);
+        // expect(data.getStats().getStats().get('Path fields')).toEqual(0);
+    });
+});
