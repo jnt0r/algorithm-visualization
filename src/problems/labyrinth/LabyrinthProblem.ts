8068c143c4d946ed9f18e9ff21bf6c34f00ba1ac
jnt0r <38696668+jnt0r@users.noreply.github.com>
2021-01-07 19:54:06 +0100

    refactor LabyrinthProblem.ts

--- src/problems/labyrinth/LabyrinthProblem.ts
+++ src/problems/labyrinth/LabyrinthProblem.ts
@@ -6,23 +6,26 @@ import GridBox from '../pathfinding/GridBox';
  *  Only difference is the generation. Instead of generating an empty grid, this problem generates a unique solvable maze
  */
 export default class LabyrinthProblem extends PathFindingProblem {
+    private sets!: GridBox[][];
+    private setsMap!: Map<number, number>;
+    private borders!: { box: GridBox; firstSetIndex: number; secondSetIndex: number }[];
+
     generate(): void {
         super.generate();
 
-        // for (let x = 0; x < this.grid.width; x++) {
-        //     for (let y = 0; y < this.grid.height; y++) {
-        //         this.grid.getElement(x, y)!.setWall();
-        //     }
-        // }
+        this.sets = [];
+        this.setsMap = new Map();
+        this.borders = [];
 
         this.generateMaze();
     }
 
     private generateMaze(): void {
-        const sets: GridBox[][] = [];
-        const map: Map<number, number> = new Map();
-        const borders: { box: GridBox; set1: number; set2: number }[] = [];
+        this.generateSetsAndBorders();
+        this.constructMaze();
+    }
 
+    private generateSetsAndBorders(): void {
         for (let x = 0; x < this.grid.width; x++) {
             for (let y = 0; y < this.grid.height; y++) {
                 // We know the element at this position must exist so we can ignore the possible return value of undefined
@@ -32,44 +35,46 @@ export default class LabyrinthProblem extends PathFindingProblem {
 
                 if (x % 2 === 0 && y % 2 === 0) {
                     gridBox.removeWall();
-                    sets.push([gridBox]);
-                    map.set(sets.length - 1, sets.length - 1);
+                    this.sets.push([gridBox]);
+                    this.setsMap.set(this.sets.length - 1, this.sets.length - 1);
                 }
                 if (x % 2 === 0 && y % 2 != 0) {
-                    borders.push({
+                    this.borders.push({
                         box: gridBox,
-                        set1: sets.length - 1,
-                        set2: sets.length,
+                        firstSetIndex: this.sets.length - 1,
+                        secondSetIndex: this.sets.length,
                     });
                 }
                 if (x % 2 != 0 && y % 2 === 0) {
                     const dx = Math.ceil(this.grid.height / 2);
                     const index1 = Math.floor(x / 2) * dx + Math.floor(y / 2);
-                    borders.push({
+                    this.borders.push({
                         box: gridBox,
-                        set1: index1,
-                        set2: index1 + dx,
+                        firstSetIndex: index1,
+                        secondSetIndex: index1 + dx,
                     });
                 }
             }
         }
+    }
 
-        while (sets[map.get(0)!].length < sets.length) {
-            const randomBorderIndex = Math.floor(Math.random() * (borders.length - 1));
-            const border = borders.splice(randomBorderIndex, 1)[0];
+    private constructMaze(): void {
+        while (this.sets[this.setsMap.get(0)!].length < this.sets.length) {
+            const randomBorderIndex = Math.floor(Math.random() * (this.borders.length - 1));
+            const border = this.borders.splice(randomBorderIndex, 1)[0];
 
-            const i1 = map.get(border.set1)!;
-            const i2 = map.get(border.set2)!;
-            if (i1 !== i2) {
+            const firstSetIndex = this.setsMap.get(border.firstSetIndex)!;
+            const secondSetIndex = this.setsMap.get(border.secondSetIndex)!;
+            if (firstSetIndex !== secondSetIndex) {
                 border.box.removeWall();
 
-                const set1 = sets[i1];
-                const set2 = sets[i2];
-                this.mergeSets(set1, set2);
+                const set1 = this.sets[firstSetIndex];
+                const set2 = this.sets[secondSetIndex] || [];
 
-                for (let i = 0; i < map.size; i++) {
-                    if (map.get(i) === i2) {
-                        map.set(i, i1);
+                this.mergeSets(set1, set2);
+                for (let i = 0; i < this.setsMap.size; i++) {
+                    if (this.setsMap.get(i) === secondSetIndex) {
+                        this.setsMap.set(i, firstSetIndex);
                     }
                 }
             }
