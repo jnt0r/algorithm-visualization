9d35c14551181e7d938c7a2723fc561f0b2680ed
jnt0r <38696668+jnt0r@users.noreply.github.com>
2021-02-26 18:12:22 +0100

    fix import

--- src/application/components/StatsComponent.ts
+++ src/application/components/StatsComponent.ts
@@ -1,5 +1,5 @@
 import ProblemStats from '../../problems/ProblemStats';
-import { ProblemStatsObserver } from '../../problems/ProblemStatsObserver';
+import ProblemStatsObserver from '../../problems/ProblemStatsObserver';
 
 export default class StatsComponent implements ProblemStatsObserver {
     private readonly statsDiv: HTMLDivElement = <HTMLDivElement>document.getElementsByClassName('stats')[0];
@@ -9,13 +9,14 @@ export default class StatsComponent implements ProblemStatsObserver {
      * @param stats the new ProblemStats object
      */
     update(stats: ProblemStats): void {
-        console.log('update');
         this.statsDiv.innerHTML = '';
 
-        stats.getStats().forEach((value, key) => {
-            const span = document.createElement('span');
-            span.innerHTML = `${key}: ${value}`;
-            this.statsDiv.appendChild(span);
-        });
+        stats.getStats().forEach(this.displayStat);
+    }
+
+    private displayStat(value: unknown, key: string): void {
+        const span = document.createElement('span');
+        span.innerHTML = `${key}: ${value}`;
+        this.statsDiv.appendChild(span);
     }
 }
