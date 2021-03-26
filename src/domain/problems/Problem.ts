ca43f15efcb8452901b81eaaa3878562236e8d47
jnt0r <38696668+jnt0r@users.noreply.github.com>
2021-03-26 15:49:33 +0100

    add comments to methods

--- src/domain/problems/Problem.ts
+++ src/domain/problems/Problem.ts
@@ -2,13 +2,32 @@ import ProblemSolver from './ProblemSolver';
 import ProblemStats from './ProblemStats';
 
 export default interface Problem<T extends Problem<T>> {
+
+    /**
+     * Should generate a new problem.
+     */
     generate(): void;
 
+    /**
+     * Should reset current problem to state before it has been solved.
+     * Should not break if called several times after reset.
+     */
     reset(): void;
 
+    /**
+     * Should solve current problem with given solver.
+     *
+     * @param solver
+     */
     solve(solver: ProblemSolver<T, unknown, unknown>): Promise<void>;
 
+    /**
+     * Should display whole problem.
+     */
     render(): void;
 
+    /**
+     * Should return stats for the current problem.
+     */
     getStats(): ProblemStats;
 }
