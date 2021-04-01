3fc7b2988610f202ea795f2d99379d51d35d0117
jnt0r <38696668+jnt0r@users.noreply.github.com>
2021-04-01 09:48:15 +0200

    fix tests

--- test/domain/problems/sorting/SortingElementConfiguration.spec.ts
+++ test/domain/problems/sorting/SortingElementConfiguration.spec.ts
@@ -2,21 +2,21 @@ import SortingElementConfiguration from '../../../../src/domain/problems/sorting
 import Point from '../../../../src/domain/renderer/Point';
 
 describe('SortingElementConfiguration', () => {
-    it('config for 2 elements on width of 100', () => {
+    it('config for 2 elements on 100x200', () => {
         const config = new SortingElementConfiguration(100, 200, 2);
 
         expect(config.getWidth()).toBe(2);
-        expect(config.getPointForIndex(0)).toEqual(new Point(43, 100));
-        expect(config.getPointForIndex(1)).toEqual(new Point(50, 100));
+        expect(config.getPointForIndex(0)).toEqual(new Point(43, 20));
+        expect(config.getPointForIndex(1)).toEqual(new Point(50, 20));
     });
 
-    it('config for 20 elements on width of 800', () => {
+    it('config for 20 elements on 800x600', () => {
         const config = new SortingElementConfiguration(800, 600, 20);
 
         expect(config.getWidth()).toBe(16);
-        expect(config.getPointForIndex(0)).toEqual(new Point(190, 100));
-        expect(config.getPointForIndex(1)).toEqual(new Point(211, 100));
-        expect(config.getPointForIndex(19)).toEqual(new Point(589, 100));
-        expect(config.getPointForIndex(20)).toEqual(new Point(610, 100));
+        expect(config.getPointForIndex(0)).toEqual(new Point(190, 60));
+        expect(config.getPointForIndex(1)).toEqual(new Point(211, 60));
+        expect(config.getPointForIndex(19)).toEqual(new Point(589, 60));
+        expect(config.getPointForIndex(20)).toEqual(new Point(610, 60));
     });
 });
